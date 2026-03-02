64a264648ba06a2db399e7d6d40852b90e1af0a5
Kirill <whirpy@yandex.ru>
2026-03-02 23:17:49 +0400

    Убрать конфликт редиректов домена

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,18 +4,8 @@ import { routing } from './i18n/routing';
 import { mapLegacyLocalePathname } from './i18n/locale-config';
 
 const intlMiddleware = createMiddleware(routing);
-const canonicalHost = 'www.artidom.art';
 
 export default function middleware(request: NextRequest) {
-  const hostname = (request.headers.get('x-forwarded-host') ?? request.headers.get('host') ?? request.nextUrl.hostname)
-    .split(':')[0];
-
-  if (hostname === 'artidom.art') {
-    const url = request.nextUrl.clone();
-    url.hostname = canonicalHost;
-    return NextResponse.redirect(url, 308);
-  }
-
   const legacyPathname = mapLegacyLocalePathname(request.nextUrl.pathname);
 
   if (legacyPathname) {
